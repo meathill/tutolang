f2b545452c8dd260ed53b7509506e377c5c5ef71
oreo <Tang.Li5@geely.com>
2023-01-17 14:18:24 +0800

    [WIP]style: format code

--- executor/vscode/src/extension.ts
+++ executor/vscode/src/extension.ts
@@ -143,63 +143,12 @@ export function activate(context: vscode.ExtensionContext) {
   };
 
   function roundRobin() {
-    const exampleCommands = [
-      {
-        type: 'OpenFile',
-        filePath: './test.js',
-        openFileOptions: {
-          selectRange: {
-            startPosition: { row: 0, col: 0 },
-            endPosition: { row: 0, col: 0 },
-          },
-          preview: false,
-          viewColumn: 1,
-        },
-      },
-      {
-        type: 'Input',
-        content: `// (语音) JavaScript 函数是被设计为执行特定任务的代码块。
-// (语音) JavaScript 函数会在某代码调用它时被执行。
-
-// 下面我将教你如何在JavaScript中定义一个函数
-function A() {
-  let a = 1;
-  return a;
-}
-
-// 下面我们去 HTML文件 中通过<script>脚本的形式引入我们刚刚写的函数`,
-        position: { row: 0, col: 0 },
-      },
-      {
-        type: 'OpenFile',
-        filePath: './test.html',
-        openFileOptions: {
-          selectRange: {
-            startPosition: { row: 10, col: 8 },
-            endPosition: { row: 10, col: 27 },
-          },
-          preview: true,
-          viewColumn: 2,
-        },
-      },
-      { type: 'MoveCursor', toPosition: { row: 17, col: 43 } },
-      {
-        type: 'Input',
-        content: `
-    <script src="./test.js"></script>`,
-        position: { row: 17, col: 43 },
-      },
-    ];
-
     let key = 0;
     let timer = setInterval(async () => {
       key = key + 1;
       const {
         data: { commands },
       } = await axios.get(`${BASE_URL}/query?key=${key}`);
-
-      console.log(commands);
-
       if (commands.length > 0) {
         clearInterval(timer);
         exec(commands);
